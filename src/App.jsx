a250a9a8754b9bf3645c9f272b5c360b1a9a9af2
Hwakeman <hwakema2@oswego.edu>
2024-02-23 00:10:14 -0500

    Created footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+
 import Form from './components/Form'
 import Output from './components/Output'
+import Footer from './components/Footer'
 
 export default function App() {
     return (
         <>
             <Form/>
             <Output/>
+            <Footer/>
         </>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p className='footer-child'>Harley Wakeman © 2024</p>
+            <p className='footer-child'><a href="https://github.com/hwakeman/cv-app"><img className='github-logo' src="public/github.png" alt="Github link"/></a></p>
+        </footer>
+    )
+}
