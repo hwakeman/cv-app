1f9a5f3c619cd8fca01c41802fedb9b327ef2997
HarleyMax <wakemanharley@yahoo.com>
2024-02-23 13:46:59 -0500

    Started creating output and fixed some styling

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -4,6 +4,16 @@ export default function Output( {submittedCv} ) {
     return (
         <div className="output">
             <h1>Output</h1>
+            { submittedCv && 
+            <>
+                <h2>{submittedCv.name}</h2>
+                <h3 className="general-info-output">
+                    <div>{submittedCv.email}</div>
+                    <div>{submittedCv.phoneNumber}</div>
+                </h3>
+            </>
+            }
         </div>
+        
     )
 }
